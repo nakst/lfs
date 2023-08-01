ab363f0ec0104eb9ab4fb3d5d285cc1d49079481
nakst <>
2023-08-01 18:20:07 +0100

    fix pacman command

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 mkdir -p /lfs
-pacman -S wget
+pacman -Sy wget
 wget --input-file=wget-list-sysv --continue --directory-prefix=sources
 LFS=/lfs LFS_SOURCES=sources LFS_SUDO= ./main.sh
