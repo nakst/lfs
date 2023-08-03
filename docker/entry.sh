6635fd074435ff0c11799b0550e419a01c720c5c
nakst <>
2023-08-03 20:53:36 +0100

    test3

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -l
 pacman -Sy --noconfirm --needed wget base-devel python
-# wget --input-file=wget-list-sysv --continue --directory-prefix=sources
-# LFS=lfs LFS_SOURCES=sources LFS_SUDO= ./main.sh
-printf '#include <stdio.h>\nint main(){printf("hello world\\\n");return 0;\n}'
-gcc -o hello hello.c
-./hello
+wget --input-file=wget-list-sysv --continue --directory-prefix=sources
+LFS=lfs LFS_SOURCES=sources LFS_SUDO= ./main.sh
