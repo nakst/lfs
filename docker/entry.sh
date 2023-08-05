ddc71c959a42bd7361ee60bdfb930298d691540d
nakst <>
2023-08-05 15:43:32 +0100

    build teak first

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,12 +1,4 @@
 #!/bin/sh -l
-
 pacman -Syu --noconfirm --needed wget base-devel python
 wget --input-file=wget-list-sysv --continue --directory-prefix=sources
-
-git clone https://github.com/nakst/teak.git
-cd teak
-gcc -o teak teak.c -pthread -ldl
-./teak build.teak
-cd ..
-
 teak/teak main.teak lfs=lfs lfsSources=sources
